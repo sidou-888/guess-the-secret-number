f8f58da6b4bfe89ea77e97c32926016f66e3d375
Sidou <sidoborane@gmail.com>
2025-06-24 14:47:15 +0100

    guess the secret number

---
+++ 00.py
@@ -0,0 +1,13 @@
+x=int(input("guess the secret number(1.100) : "))
+while x!=9 : 
+    if x<9 :
+        print ("the secret number is higher")
+    elif x>9:
+        print ("the secret number is lower")
+    
+  
+   
+    x=int(input("try again : "))
+       
+print ("the secret number is true")
+    
